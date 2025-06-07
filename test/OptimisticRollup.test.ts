8d7baf6f6c2dc16afe51a672a9d8720b979a514b
will chandler <jaskamiin@gmail.com>
2025-06-07 05:32:24 -0400

    fix incorrect structure in test file

--- test/OptimisticRollup.test.ts
+++ test/OptimisticRollup.test.ts
@@ -17,13 +17,16 @@ describe("OptimisticRollup", function() {
         await rollup.waitForDeployment();
     });
 
-    describe("Deployment", function() {});
-    describe("Deposits", function() {});
+    describe("Deployment", function() {
+        it ("Should init with correct genesis state", async function(){});
+        it ("Should have zero balances initially", async function(){});
+    });
 
-    it ("Should reject deposits of 0 eth", async function(){});
-    it ("Should handle multiple deposits from 1 user", async function(){});
-    it ("Should handle deposits from multiple users", async function(){});
-    it ("Should lock ETH in the contract", async function(){});
-
+    describe("Deposits", function() {
+        it ("Should reject deposits of 0 eth", async function(){});
+        it ("Should handle multiple deposits from 1 user", async function(){});
+        it ("Should handle deposits from multiple users", async function(){});
+        it ("Should lock ETH in the contract", async function(){});
+    });
     describe ("Account Structure", function(){});
 });

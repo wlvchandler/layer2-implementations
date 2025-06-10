fd055d6620a87fcaaa1b8deb999c85ed56c18fd2
will chandler <jaskamiin@gmail.com>
2025-06-10 08:28:22 +0000

    feat: statelib: apply transactions

--- optimistic_rollup/contracts/lib/StateLib.sol
+++ optimistic_rollup/contracts/lib/StateLib.sol
@@ -41,9 +41,45 @@ library StateLib {
         return MerkleLib.computeRoot(leaves);
     }
 
+    function applyTransaction(TransactionLib.Transaction memory txn, mapping(address => TransactionLib.Account) storage state) 
+        internal returns (TransactionLib.TransactionResult) 
+    {
+        TransactionLib.Account memory fromAccount = state[txn.from];
+        TransactionLib.Account memory toAccount = state[txn.to];
+        
+        (
+            TransactionLib.Account memory newFromAccount,
+            TransactionLib.Account memory newToAccount,
+            TransactionLib.TransactionResult result
+        ) = TransactionLib.execute(txn, fromAccount, toAccount);
+        
+        if (result == TransactionLib.TransactionResult.SUCCESS) {
+            state[txn.from] = newFromAccount;
+            state[txn.to] = newToAccount;
+        }
+        
+        return result;
+    }
+
+    function applyTransactionBatch(TransactionLib.Transaction[] memory transactions, mapping(address => TransactionLib.Account) storage state) 
+        internal returns (uint256 successCount, uint256 failureCount) 
+    {
+        successCount = 0;
+        failureCount = 0;
+        
+        for (uint256 i = 0; i < transactions.length; i++) {
+            TransactionLib.TransactionResult result = applyTransaction(transactions[i], state);
+            if (result == TransactionLib.TransactionResult.SUCCESS) {
+                successCount++;
+            } else {
+                failureCount++;
+            }
+        }
+    }
+
     function hashAccount(address account,TransactionLib.Account memory accountData) internal pure returns (bytes32) {
         return keccak256(abi.encode(account, accountData.balance, accountData.nonce));
     }
 
-    
+
 }
